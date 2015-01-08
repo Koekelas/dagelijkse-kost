49fa689b8b697dea9b45acf5b0b8c3cf8c1e7f82
Nicolas De Jaeghere <nicodeja@mac.com>
2015-01-08 13:56:04 +0100

    Added a façade for scraper and fixed the example

--- app.js
+++ app.js
@@ -2,44 +2,13 @@
 
 "use strict";
 
-var q = require("q"),
-    scraper = require("./lib/scraper");
+var site = require("./lib/scraper").site;
 
-var recipesPage = scraper.createRecipesPage();
-recipesPage.
+site.
     getRecipeUrls().
     then(function (recipeUrls) {
-        var recipePage = scraper.createRecipePage(recipeUrls[0], recipesPage.getUrl());
-        return q.all([
-            recipePage.getRecipe(),
-            recipePage.
-                getImageUrl().
-                then(function (imageUrl) {
-                    return scraper.
-                        createImage(imageUrl, recipePage.getUrl()).
-                        stringify();
-                }),
-            recipePage.
-                getRecipeVariationUrls().
-                then(function (variationUrls) {
-                    return q.all(variationUrls.map(function (variationUrl) {
-                        return scraper.
-                            createRecipePage(variationUrl, recipePage.getUrl()).
-                            getIngredients();
-                    }));
-                })
-        ]);
-    }).
-    spread(function (recipe, image, ingredientVariations) {
-        recipe.image = image;
-        recipe.ingredients = [recipe.ingredients].
-            concat(ingredientVariations).
-            sort(function (left, right) {
-                return left.servings - right.servings;
-            });
-        return recipe;
+        return site.getRecipe(recipeUrls[0]);
     }).
     then(function (recipe) {
         console.log(JSON.stringify(recipe));
-    }).
-    done();
+    });
