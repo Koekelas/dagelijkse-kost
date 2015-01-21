97c903c9a2110b63a3d6ae3e5dda2ceebd44f5fc
Nicolas De Jaeghere <nicodeja@mac.com>
2015-01-21 10:44:51 +0100

    Started adding persistence

--- app.js
+++ app.js
@@ -2,26 +2,51 @@
 
 "use strict";
 
-var q = require("q"),
-    site = require("./lib/scraper").site,
+var fs = require("fs"),
+    Pouchdb = require("pouchdb"),
+    q = require("q"),
+    archiver = require("./lib/archiver"),
 
-    recipesPage = site.createRecipesPage();
+    app = (function app() {
+        var CONFIG_PATH = "./config.json",
+            DEFAULT_CONFIG = {couchdbUrl: ""},
+            TAB_SIZE = 2,
 
-recipesPage.
-    getRecipeUrls().
-    then(function (recipeUrls) {
-        return recipesPage.createRecipePage(recipeUrls[0]);
-    }).
-    then(function (recipePage) {
-        return recipePage.
-            createRecipeImage().
-            then(function (recipeImage) {
-                return q.all([recipePage.getSlug(), recipePage.getRecipe(), recipeImage.toDataUri()]);
-            });
-    }).
-    spread(function (slug, recipe, image) {
-        console.log(slug);
-        console.log(JSON.stringify(recipe));
-        console.log(image);
-    }).
-    done();
+            readConfig = (function () {
+                var readFile = q.denodeify(fs.readFile),
+                    writeFile = q.denodeify(fs.writeFile);
+
+                return function readConfig() {
+                    return readFile(CONFIG_PATH, {encoding: "utf8"}).then(
+                        function onSuccess(config) {
+                            return JSON.parse(config);
+                        },
+                        function onFailure() {
+                            return writeFile(
+                                CONFIG_PATH,
+                                JSON.stringify(DEFAULT_CONFIG, null, TAB_SIZE)
+                            ).then(function () {
+                                return readConfig();
+                            });
+                        }
+                    );
+                };
+            }()),
+
+            run = function run() {
+                readConfig().
+                    then(function (config) {
+                        var db = new Pouchdb(config.couchdbUrl);
+                        return archiver.archiveRecipes(db);
+                    }).
+                    done();
+            },
+
+            initialise = function initialise() {
+                return {run: run};
+            };
+
+        return initialise();
+    }());
+
+app.run();
